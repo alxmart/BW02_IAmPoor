105fcda5ae0c18b85e061a06c7c360e1edc7966c
alxmart <alxmartinez@gmail.com>
2024-09-17 11:17:28 -0300

    Project created.

---
+++ BW02_IAmPoor/BW02_IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BW02_IAmPoor
+//
+//  Created by Alessandra Martinez on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
